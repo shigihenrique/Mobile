cba7f50cdef23dfdbe212caf980c0e51c2d0ac17
Henrique Shigihara <51368609+shigihenrique@users.noreply.github.com>
2021-03-17 18:01:21 -0300

    Update atividade1.dart

--- Tarefas/atividade1.dart
+++ Tarefas/atividade1.dart
@@ -23,6 +23,34 @@ int destruirPetecas(numPetecas, numAmigos) {
   return destruir;
 }
 
+int marteloThor(thor, martelo) {
+  int x_thor = thor[0];
+  int y_thor = thor[1];
+
+  int x_martelo = martelo[0];
+  int y_martelo = martelo[1];
+
+  while (x_thor != x_martelo) {
+    if (x_thor < x_martelo) {
+      x_thor++;
+      print([x_thor, y_thor]);
+    } else if (x_thor > x_martelo) {
+      x_thor--;
+      print([x_thor, y_thor]);
+    }
+  }
+
+  while (y_thor != y_martelo) {
+    if (y_thor < y_martelo) {
+      y_thor++;
+      print([x_thor, y_thor]);
+    } else if (y_thor > y_martelo) {
+      y_thor--;
+      print([x_thor, y_thor]);
+    }
+  }
+}
+
 //FUNCAO PRINCIPAL
 void main() {
   /*
@@ -30,9 +58,13 @@ void main() {
   print(leastDifference(-1, 15, 3));
   print(leastDifference(-101, 15, 99));
   print(leastDifference(21, 35, 19));
-  */
-
+  
   print(destruirPetecas(23, 4));
   print(destruirPetecas(35, 6));
   print(destruirPetecas(95, 19));
+    
+  marteloThor([5,2], [4,7]);
+  marteloThor([9,7], [11,3]);
+  marteloThor([5, 7], [-5, -3]);
+  */
 }
