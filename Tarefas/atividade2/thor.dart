6f7e520440518cc88daddc80a66bd1515fc7985c
Henrique Shigihara <51368609+shigihenrique@users.noreply.github.com>
2021-03-31 17:35:40 -0300

    Update thor.dart

--- Tarefas/atividade2/thor.dart
+++ Tarefas/atividade2/thor.dart
@@ -3,6 +3,7 @@ List marteloThor(thor, martelo) {
 
   int xThor = thor[0];
   int yThor = thor[1];
+  lista.add([xThor, yThor]);
 
   int xMartelo = martelo[0];
   int yMartelo = martelo[1];
@@ -29,9 +30,23 @@ List marteloThor(thor, martelo) {
   return lista;
 }
 
+int tam(lista) {
+  int tamanho = 0;
+  lista.forEach((element) => tamanho++);
+  return tamanho;
+}
+
+int cont = 0;
+int getCont(tamanho) {
+  if (cont < tamanho) {
+    return cont++;
+  }
+}
+
 //FUNCAO PRINCIPAL
 void main() {
   /*
+  marteloThor(thor, martelo);
   marteloThor([5,2], [4,7]);
   marteloThor([9,7], [11,3]);
   marteloThor([5, 7], [-5, -3]);
