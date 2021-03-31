a1631ea759cfc10d4fbb79f07311f103d2958d34
Henrique Shigihara <51368609+shigihenrique@users.noreply.github.com>
2021-03-31 17:35:17 -0300

    Update main.dart

--- Tarefas/atividade2/main.dart
+++ Tarefas/atividade2/main.dart
@@ -55,22 +55,55 @@ class _MyHomePageState extends State<MyHomePage> {
 List<Widget> retornaRows() {
   List lista = marteloThor([5, 2], [4, 7]);
   print(lista);
+  int tamanho = tam(lista);
+  print(tamanho);
   List<Widget> rows = [];
+
   for (int i = 0; i < 10; i++) {
-    rows.add(Column(children: retornaContainers(lista, i)));
+    rows.add(Column(children: retornaContainers(lista, i, tamanho)));
   }
+
   return rows;
 }
 
-List<Widget> retornaContainers(lista, i) {
+List<Widget> retornaContainers(lista, i, tamanho) {
   List<Widget> row = [];
 
   for (int j = 0; j < 10; j++) {
-    List valor = [i, j];
-    print(valor);
-    bool caminho = lista.contains(valor);
-    print(caminho);
-    if (caminho) {
+    bool caminho = false;
+    bool inicio = false;
+    bool fim = false;
+    for (int cont = 0; cont < tamanho; cont++) {
+      List xyLista = lista[cont];
+      print(xyLista);
+      int xLista = xyLista[0];
+      int yLista = xyLista[1];
+      if (i == xLista && j == yLista) {
+        if (cont == 0) {
+          inicio = true;
+        } else if (cont == tamanho - 1) {
+          fim = true;
+        } else {
+          caminho = true;
+        }
+        break;
+      }
+    }
+    if (inicio) {
+      row.add(Container(
+        height: 30,
+        width: 30,
+        decoration: BoxDecoration(color: Colors.yellow),
+        margin: EdgeInsets.all(1),
+      ));
+    } else if (fim) {
+      row.add(Container(
+        height: 30,
+        width: 30,
+        decoration: BoxDecoration(color: Colors.red),
+        margin: EdgeInsets.all(1),
+      ));
+    } else if (caminho) {
       row.add(Container(
         height: 30,
         width: 30,
