2252534996c32e8069a071b4c15274e49267b1ff
Henrique Shigihara <51368609+shigihenrique@users.noreply.github.com>
2021-04-23 12:36:16 -0300

    Add files via upload

---
+++ rastapp/lib/model/complete_form.dart
@@ -0,0 +1,15 @@
+class CompleteForm {
+  var checkBoxValue = false;
+  var switchValue = false;
+  var sliderValue = .3;
+  var radioValue = 1;
+  var radioValue2 = 1;
+  var bottomSheetChoice = 0;
+
+  doSomething() {
+    print("Checkbox: $checkBoxValue");
+    print("Switch: $switchValue");
+    print("Slider: $sliderValue");
+    print("Radio: $radioValue");
+  }
+}
